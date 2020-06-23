fbdb8ab6887bd15d29ba5006d042c8ddc2f0ea27
dajkam <s13033@pjwstk.edu.pl>
2020-06-23 11:54:47 +0200

    initial commit

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+function open_close_menu() {
+
+
+    if ( document.getElementById("menu").style.display !== "none"){
+        document.getElementById("menu").style.display = "none"
+    }
+    else{
+        document.getElementById("menu").style.display = "block"
+    }
+
+
+}
